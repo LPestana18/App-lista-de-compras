721a776f0c29cade621ae1d028e3b685048cb0d0
Lucas Ribeiro Pestana <68862058+LPestana18@users.noreply.github.com>
2020-08-24 20:50:25 -0300

    inserir o item na lista

--- lista de compras/app/src/main/java/com/example/listadecompras/MainActivity.kt
+++ lista de compras/app/src/main/java/com/example/listadecompras/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.listadecompras
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Implementação do adaptador
+        val produtosAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1)
+
+        // Definindo o adaptador na lista
+        list_view_produtos.adapter = produtosAdapter
+
+        // Definição do ouvinte do botão
+        btn_inserir.setOnClickListener{
+
+            // Pegando o valor digitado pelo usuário
+            val produto = txt_produto.text.toString()
+
+            // Enviado o item para a lista
+            produtosAdapter.add(produto)
+
+        }
     }
 }
